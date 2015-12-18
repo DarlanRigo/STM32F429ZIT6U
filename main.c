a6ba64faf2ca203b0b173ebbd406dac83f4b6cbf
DarlanRigo <darlanrigo@msn.com>
2015-12-18 12:33:07 -0200

    2015_12_18

---
+++ main.c
@@ -0,0 +1,33 @@
+//Includes
+#include <stm32f4xx_adc.c>
+#include <stm32f4xx_gpio.c>
+
+/*
+ * Habilitando clock ao ADC
+ */
+ADC_DeInit();
+/*
+ * Atualizar estrutura antes de GPIO_DeInit()
+ */
+
+
+
+
+/*
+ * Configurando dos pinos
+ */
+
+GPIO_DeInit(GPIO_TypeDef);
+
+
+
+
+
+
+int main(void)
+{
+
+    while(1)
+    {
+    }
+}
